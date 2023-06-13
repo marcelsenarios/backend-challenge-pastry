c20425c54041d57c76b7f9601a94fc16e88057cb
Marcel Sena Rios <carlosmarcel@brq.com>
2023-06-13 17:08:16 -0300

    feat: create migration Clientes

---
+++ database/migrations/2023_06_13_195755_create_clientes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone');
+            $table->date('data_de_nascimento');
+            $table->string('endereco');
+            $table->string('complemento')->nullable();
+            $table->string('bairro');
+            $table->string('cep');
+            $table->timestamp('data_de_cadastro')->default(now());
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
